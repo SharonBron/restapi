e49e6ba4e0a4a6c212e2f92d1ea3f25bf12921b5
SharonBron <7sharonbron7@gmail.com>
2024-11-23 17:55:08 +0200

    server.js

---
+++ server.js
@@ -0,0 +1,36 @@
+require('dotenv').config(); // load environment variables
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const postRoutes = require('./routes/post_routes');
+const commentRoutes = require('./routes/comment_routes');
+
+const app = express();
+
+// middleware
+app.use(bodyParser.json());
+
+// routes
+app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes);
+
+// connect to mongodb
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected...');
+    } catch (err) {
+        console.error(`Error connecting to MongoDB: ${err.message}`);
+        process.exit(1); //exit the process with a failure
+    }
+};
+
+// start server
+const PORT = process.env.PORT;
+
+connectDB().then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
